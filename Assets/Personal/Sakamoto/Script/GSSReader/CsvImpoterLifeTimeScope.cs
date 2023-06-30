9876d8238f64f35657dc6b1368143d21f3569f7f
sakamotosyo <syo10150000@gmail.com>
2023-06-30 15:29:43 +0900

    [Add]スプレットシートを読み込んでくるScriptの作成

---
+++ Assets/Personal/Sakamoto/Script/GSSReader/CsvImpoterLifeTimeScope.cs
@@ -0,0 +1,12 @@
+// 日本語対応
+using UnityEngine;
+using VContainer;
+using VContainer.Unity;
+
+public class CsvImpoterLifeTimeScope : LifetimeScope
+{
+    protected override void Configure(IContainerBuilder builder)
+    {
+        
+    }
+}
